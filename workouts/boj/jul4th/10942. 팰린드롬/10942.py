a8fb7e3feb096b0a0ad81458eeb0e9a802f0761c
P.B <97777262+PhoneticsBug@users.noreply.github.com>
2023-07-31 01:24:23 +0900

    .

---
+++ workouts/boj/jul4th/10942. 팰린드롬/10942.py
@@ -0,0 +1,45 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+# nums = list(map(int, input().split()))
+nums = "".join(map(str, input().split()))
+dp = [[0]*(n) for _ in range(n)]
+
+for i in range(n):
+    for j in range(i, n):
+        if nums[i : j+1] == nums[i : j+1][::-1]:
+            dp[i][j] = 1
+
+# print(*dp, sep='\n')
+m = int(input())
+for _ in range(m):
+    start, end = map(int, input().split()) 
+    print(dp[start-1][end-1])
+
+# ------------------------------------------
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+nums = list(map(int, input().split())) #"".join(map(str, input().split()))
+dp = [[0]*(n) for _ in range(n)]
+
+for i in range(n): # 단독
+    dp[i][i] = 1
+
+for i in range(n-1): # 거리가 2인 경우
+    if nums[i] == nums[i+1]:
+        dp[i][i+1] = 1
+
+for i in range(n-2): # 그 외의 경우
+    for j in range(n-2-i):
+        k = i+j+2
+        if nums[j] == nums[k] and dp[j+1][k-1] == 1:
+            # 처음과 끝이 같고 그 사이도 팰린드롬이라면 
+            dp[j][k] = 1
+
+m = int(input())
+for i in range(m):
+    s, e = map(int, input().split())
+    print(dp[s-1][e-1])
---
+++ workouts/boj/jul4th/1106. 호텔/1106.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+c, n = map(int, input().split()) # 고객의 수, 도시의 수
+city = [list(map(int, input().split())) for _ in range(n)] 
+#내부 인덱스 0은 금액, 1은 늘어나는 고객 수
+
+dp = [2e9 for _ in range(10001)]
+dp[0] = 0
+
+for cost, customer in city:
+    for i in range(customer, 10001):
+        dp[i] = min(dp[i - customer] + cost, dp[i])
+print(min(dp[c:]))
