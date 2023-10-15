cc5f6d72d088da171e33e845192990713eb10d11
P.B <97777262+PhoneticsBug@users.noreply.github.com>
2023-10-16 02:10:07 +0900

    .

---
+++ workouts/October/week2/15989. 1, 2, 3 더하기 4/15989.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+def solution(numbers):
+    answer = 0
+    dp = [[0]*(t+1) for _ in range(2)]
+    
+
+
+    return answer
+
+
+
+if __name__ == "__main__":
+    t = int(input())
+    numbers = [map(int, input().split())]
+    
+    for number in numbers:
+        print(solution(number))
