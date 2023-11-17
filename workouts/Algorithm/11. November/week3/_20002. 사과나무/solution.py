cf6e49e153350e59864c7c5df1f8d3a8a911c937
P.B <97777262+PhoneticsBug@users.noreply.github.com>
2023-11-18 03:00:19 +0900

    .

--- workouts/Algorithm/11. November/week3/_20002. 사과나무/solution.py
+++ workouts/Algorithm/11. November/week3/_20002. 사과나무/solution.py
@@ -26,3 +26,30 @@ def solution(n, harvest):
 
 print(solution(n, harvest))
 
+# 시간 초과
+
+###################################################################
+
+import sys
+input = sys.stdin.readline
+
+n = int(input()) # 과수원의 크기 (정사각형)
+pre = [[-1001]*(n+1) for _ in range(n+1)]
+
+# (i, j)까지의 구간합 계산
+for i in range(1, n+1):
+    arr = list(map(int, input().split()))
+    for j in range(1, n+1):
+        pre[i][j] = pre[i][j-1] + pre[i-1][j] - pre[i-1][j-1] + arr[j-1]
+
+max_profit = pre[0][0]
+
+# 정사각형 커팅
+for k in range(n):
+    for i in range(1, n-k+1):
+        for j in range(1, n-k+1):
+            # 시작점 (i, j), 끝점 (i+k, j+k)인 정사각형의 총이익
+            profit = pre[i+k][j+k] - pre[i-1][j+k] - pre[i+k][j-1] + pre[i-1][j-1]
+            max_profit = max(max_profit, profit)
+
+print(max_profit)
