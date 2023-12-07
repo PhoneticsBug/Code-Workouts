c6313cf18633f7943b14c4a15f800967023921e9
P.B <97777262+PhoneticsBug@users.noreply.github.com>
2023-12-07 16:09:57 +0900

    .

--- workouts/Algorithm/12. December/week1/1105. 팔/solution.py
+++ workouts/Algorithm/12. December/week1/1105. 팔/solution.py
@@ -4,3 +4,27 @@ input = sys.stdin.readline
 l, r = map(int, input().split())
 length = len(str(r)) # 최대길이
 
+for num in range(l, r+1):
+    if length == 0:
+        break
+    elif num > l and length > len(str(r)):
+        continue
+    else:
+        length = min(length, str(num).count('8'))
+
+print(length)
+
+#################
+
+import sys
+input = sys.stdin.readline
+
+l, r = map(int, input().split())
+length = len(str(r)) # 최대길이
+
+for num in range(l, r+1):
+    if length == 0:
+        break
+    length = min(length, str(num).count('8'))
+
+print(length)
