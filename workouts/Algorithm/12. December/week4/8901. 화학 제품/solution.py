2423bca066a563af04102ec7ed72eac31db461e0
phoneticsbug <swwe1000@gmail.com>
2023-12-19 17:51:15 +0900

    .

--- workouts/Algorithm/12. December/week4/8901. 화학 제품/solution.py
+++ workouts/Algorithm/12. December/week4/8901. 화학 제품/solution.py
@@ -1,3 +1,22 @@
 import sys
 input = sys.stdin.readline
 
+t = int(input())
+
+# 화학약품별 가격차이가 있는 경우
+# 값이 가장 큰 약품부터 시작해서 줄여나감
+# 없는 경우
+# 순서대로 줄여나감
+
+
+def solution():
+    a, b, c = map(int, input().split())
+    ab, bc, ca = map(int, input())
+    cost = 0
+
+    
+
+
+
+for _ in range(t):
+    solution()
